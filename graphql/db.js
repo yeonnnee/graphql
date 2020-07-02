24b421bfd7cd9dbbf4f47aacf26ec476d4765005
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-07-02 16:49:21 +0900

    create mutation

--- graphql/db.js
+++ graphql/db.js
@@ -8,3 +8,31 @@ export const getMoive = (id) => {
     const movie = movies.filter(movie => movie.id === id);
     return movie[0];
 };
+
+export const addMovie = (title, eneTitle, poster, year, country, genre, run, summary, actor, producer ) => {
+    const newMovie = {
+        id: movies.length + 1,
+        title,
+        eneTitle,
+        poster,
+        year,
+        country,
+        genre,
+        run,
+        summary,
+        actor,
+        producer,
+    }
+    movies.push(newMovie);
+    return newMovie
+}
+
+export const deleteMovie = (id) => {
+    const result = moives.fileter(movie => movie.id !== id);
+    if(movies.length > result.length) {
+        movies = result;
+        return true;
+    }else {
+        return false;
+    }
+}
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,10 +1,18 @@
-import { getMovies, getMovie } from "./db";
+import { getMovies, getMovie, addMovie, deleteMovie } from "./db";
 
 const resolvers = {
     Query: {
         movies: () => getMovies(),
         movie: (_, { id }) => getMovie(id),
     },
+    Mutation: {
+        addMovie: (_, {
+            title, eneTitle, poster, year, country, genre, run, summary, actor, producer 
+        }) => addMovie(
+            title, eneTitle, poster, year, country, genre, run, summary, actor, producer 
+        ),
+        deleteMovie: (_, { id }) => deleteMovie(id),
+    }
 }
 
 

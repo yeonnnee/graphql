e7b7faf35026d66f7fb0191d65e7135ee0abd10f
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-07-02 16:09:20 +0900

    set schema

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import { GraphQlServer } from 'graphql-yoga';
 
-const server = new GraphQlServer ({
 
+const server = new GraphQlServer ({
+    typeDefs: "graphql/schema.graphql",
+    
 });
 
 server.start(() => console.log("Graphql Server Running"));
